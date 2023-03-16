af9958c9a3912deb16ad71a5d9ad491cfa6a6984
sekharjavvadi <sekharatece@gmail.com>
2023-03-16 13:37:07 +0530

    slide 1 completed in porfolio, working on about me slide

--- src/app/naidusunkari/page.js
+++ src/app/naidusunkari/page.js
@@ -1,5 +1,14 @@
+import AboutMe from "@/components/portfolio_ceo/aboutme";
+import Slide1 from "@/components/portfolio_ceo/slide1";
 import React from "react";
 
-export default function NaiduSunkari() {
-  return <div>naidu_sunkari</div>;
-}
+const Portfolio = () => {
+  return (
+    <div className="w-[100vw] min-h-[100vh] h-fit gil-reg">
+      <Slide1 />
+      <AboutMe />
+    </div>
+  );
+};
+
+export default Portfolio;
