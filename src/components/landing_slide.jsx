822e7d3d305a2a3d256ebe9fa37449b1a93269b4
hemanthveeranala <hemanthveeranala123@gmail.com>
2023-03-18 12:51:54 +0530

    images changed to new folder in web3-spotmies firebase project

--- src/components/landing_slide.jsx
+++ src/components/landing_slide.jsx
@@ -4,19 +4,17 @@ import HoverButton from "./reusableComponent/hoverButton";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 const video =
-  "https://firebasestorage.googleapis.com/v0/b/schemax-875a7.appspot.com/o/video2.mp4?alt=media&token=e89efa63-b71f-4a0a-a8fb-3159d9b0a970";
+  "https://firebasestorage.googleapis.com/v0/b/web3-spotmies.appspot.com/o/schemax%2Fvideo2.mp4?alt=media&token=d11e2881-1e5a-4952-8d65-97fbed0fb6f4";
 // "https://firebasestorage.googleapis.com/v0/b/schemax-875a7.appspot.com/o/video.mp4?alt=media&token=e5bab7cd-0077-4974-a2b2-88dd82358a02";
 
 const LandingSlide = () => {
   return (
     <section
       className="h-screen w-full flex flex-col justify-center items-center relative"
-      id="home"
-    >
+      id="home">
       <div
         className="absolute inset-0 bg-primary opacity-40 w-full h-full rounded-md "
-        style={{ zIndex: "9" }}
-      ></div>
+        style={{ zIndex: "9" }}></div>
       <div className="absolute top-0 left-0 h-full w-full z-10">
         <Navbarr />
       </div>
@@ -73,18 +71,15 @@ export const Navbarr = () => {
         <div className="flex items-center md:hidden">
           <button
             className="text-white hover:text-secundary focus:outline-none"
-            aria-label="Menu"
-          >
+            aria-label="Menu">
             <svg
               viewBox="0 0 20 20"
               fill="currentColor"
-              className="menu w-6 h-6"
-            >
+              className="menu w-6 h-6">
               <path
                 fillRule="evenodd"
                 d="M2 5a1 1 0 011-1h14a1 1 0 110 2H3a1 1 0 01-1-1zm0 5a1 1 0 011-1h14a1 1 0 110 2H3a1 1 0 01-1-1zm1 4a1 1 0 100 2h14a1 1 0 100-2H3z"
-                clipRule="evenodd"
-              ></path>
+                clipRule="evenodd"></path>
             </svg>
           </button>
         </div>
@@ -92,38 +87,32 @@ export const Navbarr = () => {
           <a
             // href="/"
             onClick={() => navigation("services")}
-            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer"
-          >
+            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer">
             Services
           </a>
           <a
             onClick={() => navigation("industries")}
-            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer"
-          >
+            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer">
             Industries
           </a>
           <a
             onClick={() => navigation("career")}
-            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer"
-          >
+            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer">
             Career
           </a>
           <a
             onClick={() => navigation("articles")}
-            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer"
-          >
+            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer">
             Blogs
           </a>
           <a
             onClick={() => navigation("aboutUs")}
-            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer"
-          >
+            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer">
             About Us
           </a>
           <a
             onClick={() => navigation("contactUs")}
-            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer"
-          >
+            className="text-white  mx-4 border-b-2 border-transparent hover:border-secundary cursor-pointer">
             Contact Us
           </a>
         </div>
